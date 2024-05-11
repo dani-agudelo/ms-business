225c7182063b13f88745aeaaebec7281f355b2c0
Daniell-Estrada <nelson.1702013546@ucaldas.edu.co>
2024-05-11 18:47:49 -0500

    Beneficiary inheritance and customer class owner, relationships

---
+++ app/Models/Customer.ts
@@ -0,0 +1,25 @@
+import { DateTime } from "luxon";
+import { BaseModel, column, HasMany, hasMany } from "@ioc:Adonis/Lucid/Orm";
+import Beneficiary from "./Beneficiary";
+import Holder from "./Holder";
+
+export default class Customer extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number;
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime;
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime;
+
+  @hasMany(() => Holder, {
+    foreignKey: "customer_id",
+  })
+  public holders: HasMany<typeof Holder>;
+
+  @hasMany(() => Beneficiary, {
+    foreignKey: "customer_id",
+  })
+  public beneficiaries: HasMany<typeof Beneficiary>;
+}
---
+++ app/Models/Holder.ts
@@ -0,0 +1,32 @@
+import { DateTime } from "luxon";
+import {
+  BaseModel,
+  BelongsTo,
+  belongsTo,
+  column,
+  HasMany,
+  hasMany,
+} from "@ioc:Adonis/Lucid/Orm";
+import Customer from "./Customer";
+import Beneficiary from "./Beneficiary";
+
+export default class Holder extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number;
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime;
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime;
+
+  @belongsTo(() => Customer, {
+    foreignKey: "customer_id",
+  })
+  public customer: BelongsTo<typeof Customer>;
+
+  @hasMany(() => Beneficiary, {
+    foreignKey: "holder_id",
+  })
+  public beneficiaries: HasMany<typeof Beneficiary>;
+}

04474ab1722e21d241f2028abb8c10d09b6d09b8
Daniell-Estrada <nelson.1702013546@ucaldas.edu.co>
2024-05-13 12:38:53 -0500

    HeadquarterValidator create.

---
+++ app/Validators/HeadquarterValidator.ts
@@ -0,0 +1,22 @@
+import { schema, CustomMessages, rules } from "@ioc:Adonis/Core/Validator";
+import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+
+export default class HeadquarterValidator {
+  constructor(protected ctx: HttpContextContract) {}
+
+  public schema = schema.create({
+    headquarter_id: schema.number.optional([
+      rules.exists({
+        table: "headquarters",
+        column: "id",
+        where: { id: this.ctx.request.input("id") },
+      }),
+    ]),
+    name: schema.string([rules.maxLength(255)]),
+    adress: schema.string([rules.maxLength(255)]),
+    city: schema.string(),
+    phone: schema.string([rules.mobile({ locale: ["es-CO"] })]),
+  });
+
+  public messages: CustomMessages = {};
+}

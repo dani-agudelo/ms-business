b9b9ed865879dd7acca73cba86b2d4e538a38dd6
JuandaMcMg <juan.marin28310@ucaldas.edu.co>
2024-05-13 22:40:56 -0500

    ServiceExecution route updated / tsconfig modified

--- start/routes/service_execution.ts
+++ start/routes/service_execution.ts
@@ -1,8 +1,8 @@
 import Route from "@ioc:Adonis/Core/Route";
 Route.group(() => {
-  Route.get("/", "ServiceExecutionsController.find");
-  Route.get("/:id", "ServiceExecutionsController.find");
-  Route.post("/", "ServiceExecutionsController.create");
-  Route.put("/:id", "ServiceExecutionsController.update");
-  Route.delete("/:id", "ServiceExecutionsController.delete");
-}).prefix("/service_executions");
+  Route.get("/service_executions", "ServiceExecutionsController.find");
+  Route.get("/service_executions/:id", "ServiceExecutionsController.find");
+  Route.post("/service_executions", "ServiceExecutionsController.create");
+  Route.put("/service_executions/:id", "ServiceExecutionsController.update");
+  Route.delete("/service_executions/:id", "ServiceExecutionsController.delete");
+})

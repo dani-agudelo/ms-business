7f1553ce76fd63396bf7bbd3738927e4219c92c9
Daniell-Estrada <nelson.1702013546@ucaldas.edu.co>
2024-06-15 00:47:11 -0500

    Function find beneficiaries by Owner created on Beneficiaries Controller.

--- app/Controllers/Http/BeneficiariesController.ts
+++ app/Controllers/Http/BeneficiariesController.ts
@@ -1,12 +1,18 @@
+import { inject } from "@adonisjs/core/build/standalone";
 import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
 
 import Beneficiary from "App/Models/Beneficiary";
+import UserService from "App/services/user_service";
 import BeneficiaryValidator from "App/Validators/BeneficiaryValidator";
 
+@inject([UserService])
 export default class BeneficiariesController {
+  constructor(protected userService: UserService) {}
+
   public async find({ request, params }: HttpContextContract) {
     if (params.id) {
       let theBeneficiaty: Beneficiary = await Beneficiary.findOrFail(params.id);
+      await theBeneficiaty.load("customer");
       return theBeneficiaty;
     } else {
       const data = request.all();
@@ -19,6 +25,27 @@ export default class BeneficiariesController {
       }
     }
   }
+
+  public async findByOwner({ params }: HttpContextContract) {
+    const beneficiaries: Beneficiary[] = await Beneficiary.query()
+      .where("owner_id", params.id)
+      .preload("customer");
+
+    return await Promise.all(
+      beneficiaries.map(async (beneficiary: Beneficiary) => {
+        let user = await this.userService.getUserById(
+          beneficiary.customer.user_id,
+        );
+
+        return {
+          name: user.data.name,
+          email: user.data.email,
+          ...beneficiary.toJSON(),
+        };
+      }),
+    );
+  }
+
   public async create({ request }: HttpContextContract) {
     const body = await request.validate(BeneficiaryValidator);
     const theBeneficiaty: Beneficiary = await Beneficiary.create(body);

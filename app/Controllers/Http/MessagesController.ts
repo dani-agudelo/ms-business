2b7a7980888b10b247c3a5a6be07c184e8026af6
Daniell-Estrada <nelson.1702013546@ucaldas.edu.co>
2024-06-15 03:26:20 -0500

    Function find messages by chat.

--- app/Controllers/Http/MessagesController.ts
+++ app/Controllers/Http/MessagesController.ts
@@ -1,42 +1,45 @@
-import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import Message from 'App/Models/Message';
-import MessageValidator from 'App/Validators/MessageValidator';
+import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+import Message from "App/Models/Message";
+import MessageValidator from "App/Validators/MessageValidator";
 
 export default class MessagesController {
-
-    public async find({ request, params }: HttpContextContract) {
-        if (params.id) {
-            let theMessage: Message = await Message.findOrFail(params.id);
-            return theMessage;
-        } else {
-            const data = request.all()
-            if ("page" in data && "per_page" in data) {
-                const page = request.input('page', 1);
-                const perPage = request.input("per_page", 20);
-                return await Message.query().paginate(page, perPage)
-            } else {
-                return await Message.query()
-            }
-
-        }
-
+  public async find({ request, params }: HttpContextContract) {
+    if (params.id) {
+      let theMessage: Message = await Message.findOrFail(params.id);
+      return theMessage;
+    } else {
+      const data = request.all();
+      if ("page" in data && "per_page" in data) {
+        const page = request.input("page", 1);
+        const perPage = request.input("per_page", 20);
+        return await Message.query().paginate(page, perPage);
+      } else {
+        return await Message.query();
+      }
     }
-    public async create({ request }: HttpContextContract) {
-        const body = await request.validate(MessageValidator);
-        const theMessage: Message = await Message.create(body);
-        return theMessage;
-    }
+  }
 
-    public async update({ params, request }: HttpContextContract) {
-        const theMessage: Message = await Message.findOrFail(params.id);
-        const data = request.body();
-        theMessage.merge(data);
-        return await theMessage.save();
-    }
+  public async findByChat({ params }: HttpContextContract) {
+    return await Message.query().where("chat_id", params.id);
+  }
 
-    public async delete({ params, response }: HttpContextContract) {
-        const theMessage: Message = await Message.findOrFail(params.id);
-        response.status(204);
-        return await theMessage.delete();
-    }
+  public async create({ request }: HttpContextContract) {
+    console.log(request.body());
+    const body = await request.validate(MessageValidator);
+    const theMessage: Message = await Message.create(body);
+    return theMessage;
+  }
+
+  public async update({ params, request }: HttpContextContract) {
+    const theMessage: Message = await Message.findOrFail(params.id);
+    const data = request.body();
+    theMessage.merge(data);
+    return await theMessage.save();
+  }
+
+  public async delete({ params, response }: HttpContextContract) {
+    const theMessage: Message = await Message.findOrFail(params.id);
+    response.status(204);
+    return await theMessage.delete();
+  }
 }
--- start/routes/message.ts
+++ start/routes/message.ts
@@ -1,9 +1,9 @@
-import Route from '@ioc:Adonis/Core/Route'
+import Route from "@ioc:Adonis/Core/Route";
 Route.group(() => {
-    Route.get("/messages", "CityController.find");
-    // en el controlador se va a buscar el id (params.id) debe ser el nombre igual
-    Route.get("/messages/:id", "MessagesController.find");
-    Route.post("/messages", "MessagesController.create");
-    Route.put("/messages/:id", "MessagesController.update");
-    Route.delete("/messages/:id", "MessagesController.delete");
-})
+  Route.get("/", "MessagesController.find");
+  Route.get("/chat/:id", "MessagesController.findByChat");
+  Route.get("/:id", "MessagesController.find");
+  Route.post("/", "MessagesController.create");
+  Route.put("/:id", "MessagesController.update");
+  Route.delete("/:id", "MessagesController.delete");
+}).prefix("/messages");

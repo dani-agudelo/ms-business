2cc480543c652116f7f474ab8ae4c01b1401e678
Daniell-Estrada <nelson.1702013546@ucaldas.edu.co>
2024-06-18 14:08:08 -0500

    Update controller subscriptions

--- start/routes/customer.ts
+++ start/routes/customer.ts
@@ -2,10 +2,10 @@ import Route from "@ioc:Adonis/Core/Route";
 Route.group(() => {
   Route.get("/", "CustomersController.find");
   Route.get("/without_owner", "CustomersController.findWithoutOwner");
+  Route.get("/user/:user_id", "CustomersController.findCustomerByUser");
   Route.get("/:id", "CustomersController.find");
   Route.post("/", "CustomersController.create");
   Route.put("/:id", "CustomersController.update");
   Route.delete("/:id", "CustomersController.delete");
-})
-  .prefix("/customers")
-  //.middleware(["security"]);
+}).prefix("/customers");
+//.middleware(["security"]);
